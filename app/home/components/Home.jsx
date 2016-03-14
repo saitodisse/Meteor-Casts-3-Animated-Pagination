84a6d1c440597243ce3f7246707eff6ec0372489
Floyd Price <f.price@floydprice.com>
2016-03-14 17:02:31 +0000

    reduced the size of the tweet to allow for more on the page at the cast resolution

--- app/home/components/Home.jsx
+++ app/home/components/Home.jsx
@@ -4,12 +4,14 @@ import Tweet from './Tweet'
 class Home extends React.Component {
   render() {
     return (
-      <div className="tweets">
-        {this.props.tweets.map((tweet) => {
-          return (
-            <Tweet tweet={tweet}/>
-          )
-        })}
+      <div className="tweets-container">
+        <div className="tweets">
+          {this.props.tweets.map((tweet) => {
+            return (
+              <Tweet tweet={tweet}/>
+            )
+          })}
+        </div>
       </div>
     )
   }
